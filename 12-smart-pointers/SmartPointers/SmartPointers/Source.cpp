63b3d86b86c427c7f0d19f2fabca406b701b2b57
Saad.Ur.Rehman <saad.ur.rehman77@gmail.com>
2022-08-13 11:43:47 +0500

    Adds weak pointer example to smartpointers solution

--- 12-smart-pointers/SmartPointers/SmartPointers/Source.cpp
+++ 12-smart-pointers/SmartPointers/SmartPointers/Source.cpp
@@ -24,10 +24,15 @@ int main()
 		std::shared_ptr<Entity> sharedEntity; // this pointer uses reference count
 		{
 			std::shared_ptr<Entity> sharedEntity2 = std::make_shared<Entity>();
-			sharedEntity = sharedEntity2;
+			sharedEntity = sharedEntity2; // the pointer will only die when all of its references have died.
 		}
 
-		std::weak_ptr<Entity> weakEntity; // weak pointer
+		// Weak pointers are used when we do not want the ownership of a pointer.
+		std::weak_ptr<Entity> weakEntity; // assigning a shared pointer to a weak pointer does not increase the ref count.
+		{
+			std::shared_ptr<Entity> sharedEntity2 = std::make_shared<Entity>();
+			weakEntity = sharedEntity2; // this will die once outside this block because the weak pointer will not keep it alive.
+		}
 	}
 
 	std::cin.get();
