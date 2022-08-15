70b32bbf87bbee43362303333ced405be1e2552c
Saad.Ur.Rehman <saad.ur.rehman77@gmail.com>
2022-08-15 10:19:17 +0500

    Adds libraries example

---
+++ 16-libraries-static-dynamic/Libraries-Linking/Engine/src/Engine.cpp
@@ -0,0 +1,10 @@
+#include "Engine.h"
+#include <iostream>
+
+namespace engine
+{
+	void PrintMessage()
+	{
+		std::cout << "Engine is ready..." << std::endl;
+	}
+}
---
+++ 16-libraries-static-dynamic/Libraries-Linking/Libraries-Linking/src/Application.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Engine.h"
+
+int main()
+{
+	engine::PrintMessage();
+
+	std::cin.get();
+	return 0;
+}
